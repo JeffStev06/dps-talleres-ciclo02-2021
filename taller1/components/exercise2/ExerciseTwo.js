6589c458c8a4f4c11de8c0d11e454e57e30414d0
Daniel <josedaniel9839@gmail.com>
2021-09-05 01:56:47 -0600

    Estructura de la vista

--- taller1/components/exercise2/ExerciseTwo.js
+++ taller1/components/exercise2/ExerciseTwo.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text,TextInput,Button} from 'react-native'
 
 export default function ExerciseOne() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Ejer Dos</Text>
+      <View style={{fontWeigth:'Bold',fontSize=16}} >
+          <Text>Ingresa tu Nombre y tu Salario</Text>
+      </View>
+      <View style={{paddingLeft:10}}>        
+        <TextInput type='name' keyboardType='default' placeholder="Tu Nombre" style={styles.input}  
+                  onEndEditing={(num)=> validar(setA,num.nativeEvent.text)} />
+        <TextInput keyboardType='numeric' placeholder="Tu Salario" style={styles.input} 
+                  onEndEditing={(num)=> validar(setB,num.nativeEvent.text)} />
+      </View>
+      <View>      
+        <Button
+          title="Calcular"
+          color="#841584"
+          onPress={response}
+          />
+      </View>
     </View>
   );
 }
