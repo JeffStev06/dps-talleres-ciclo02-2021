7bd0eb32bbd0e263a6e4ea9fbd71e1058e9f1749
JeffStev06 <jeffrey.stv06@gmail.com>
2021-10-09 18:47:45 -0600

    Ejercicio 4 y 5

--- taller2/components/exercise4/ExerciseFour.js
+++ taller2/components/exercise4/ExerciseFour.js
@@ -1,10 +1,76 @@
-import React from 'react'
-import {View,Text} from 'react-native'
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput, StyleSheet, Button, ToastAndroid } from "react-native";
 
-export default function ExerciseFive(){
-    return(
-        <View style={{flex:1, alignItems:'center',justifyContent:'center'}}>
-            <Text>Ejercicio  4</Text>
-        </View>
-    );
+export default function ExerciseFour() {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [x, setX] = useState(0);
+
+  const validar = (setNumber, valor) => {
+	  if (valor == "") {
+      setNumber(parseInt(0));
+    } else {
+      setNumber(parseInt(valor));
+    }
+  };
+  
+  const showToast = () => {
+    ToastAndroid.show("No es posible dividir entre cero", ToastAndroid.SHORT);
+  };
+
+  const response = () => {
+    if (b == 0) {
+      showToast();
+      setX("⛔");
+    } else {
+      setX(a / b);
+    }
+  };
+
+  return (
+    <View style={{ backgroundColor: "white", flex: 1, alignItems: "center" }}>
+      <View style={{ padding: 20 }}>
+        <Text style={{ fontSize: 16 }}>División de dos números</Text>
+      </View>
+      <View style={{ paddingLeft: 10 }}>
+        <TextInput
+          keyboardType="numeric"
+          placeholder="Primer número"
+          style={styles.input}
+          onChangeText={(num) => validar(setA, num)}
+        />
+        <TextInput
+          keyboardType="numeric"
+          placeholder="Segundo número"
+          style={styles.input}
+          onChangeText={(num) => validar(setB, num)}
+        />
+      </View>
+      <View>
+        <Button title="Calcular" color="#841584" onPress={response} />
+      </View>
+      <Text style={{ fontSize: 16, paddingTop: 40 }}>
+        Resultado de la División:
+      </Text>
+      <View style={{ paddingTop: 40, fontWeigth: "bold" }}>
+        <Text style={{ fontSize: 16 }}>
+          {a} / {b} = {x}
+        </Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
+  },
+  formula: {
+    height: 150,
+    width: 300,
+  },
+});
--- taller2/components/exercise5/ExerciseFive.js
+++ taller2/components/exercise5/ExerciseFive.js
@@ -1,10 +1,71 @@
-import React from 'react'
-import {View,Text} from 'react-native'
+import React, { useState } from 'react'
+import { View,Text, StyleSheet, TextInput, Button } from 'react-native'
 
-export default function ExerciseFive(){
-    return(
-        <View style={{flex:1, alignItems:'center',justifyContent:'center'}}>
-            <Text>Ejercicio  5</Text>
-        </View>
-    );
+export default function ExerciseFive() {
+  const [f, setF] = useState(0);
+  const [res, setRes] = useState(0);
+  const [def, setDef] = useState("");
+
+  const calculate = () => {
+    let toCalc = f;
+    // Si el ingresado es negativo
+    if (toCalc < 0) {
+      toCalc = toCalc * -1;
+    }
+    
+    let definition = toCalc;
+    let response = toCalc;
+
+    if (toCalc != 0) {
+      for (let i = (toCalc-1); i > 0; i--) {
+        response = response * i;
+        definition = definition + " x "+ i;
+      }
+    } else {
+      response = 1;
+    }
+    setDef(definition);
+    setRes(response);
+  }
+
+  return (
+    <View style={{ backgroundColor: "white", flex: 1, alignItems: "center" }}>
+      <View style={{ padding: 20 }}>
+        <Text style={{ fontSize: 16 }}>Factorial de un número</Text>
+      </View>
+      <View style={{ paddingLeft: 10 }}>
+        <TextInput
+          keyboardType="numeric"
+          placeholder="Número"
+          style={styles.input}
+          onChangeText={(num) => setF(num)}
+        />
+      </View>
+      <View>
+        <Button title="Calcular" color="#841584" onPress={calculate} />
+      </View>
+      <Text style={{ fontSize: 16, paddingTop: 40 }}>
+        Resultado del Factorial: { f }!
+      </Text>
+      <View style={{ paddingTop: 40, fontWeigth: "bold" }}>
+        <Text style={{ fontSize: 16 }}>
+          { def } = { res }
+        </Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
+  },
+  formula: {
+    height: 150,
+    width: 300,
+  },
+});
